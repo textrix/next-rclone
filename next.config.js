ea6ac1138cf097d669d46000ede4cbe40c28adf3
Textrix <t3xtrix@gmail.com>
2023-12-09 10:57:19 +0900

    Implemented API proxy for RCLONE as a rewrite.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+        return [{
+            source: '/api/rclone/:path*',
+            destination: process.env.RCD_URL + '/:path*',
+        },];
+    },
     env: {
         RCD_URL: process.env.RCD_URL,
     },
